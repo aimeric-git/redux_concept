55b99b7daf3025a172c69926f7be9831021dcddc
Aimeric <aimericjjb@gmail.com>
2022-08-30 21:04:18 +0300

    create mapStateToProps and mapDispatchToProps

--- src/components/PhoneComponent.js
+++ src/components/PhoneComponent.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { buyPhone } from "../redux/phone/actionPhone";
 
-const PhoneComponent = () => {
-    return(
-        <div className='container'>
-            <p>Nombre de téléphone: <span>10</span></p>
-            <button>Acheter</button>
-        </div>
-    )
-}
+const PhoneComponent = (props) => {
+  console.log(props);
+  return (
+    <div className="container">
+      <p>
+        Nombre de téléphone: <span>{props.phones}</span>
+      </p>
+      <button onClick={() => props.buyPhone()}>Acheter</button>
+    </div>
+  );
+};
 
-export default PhoneComponent;
+const mapStateToProps = (state) => {
+  return {
+    phones: state.phones,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyPhone: () => dispatch(buyPhone()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PhoneComponent);
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,3 +4,4 @@ import phoneReducer from "./phone/reducerPhone";
 const store = createStore(phoneReducer);
 
 export default store;
+ 
